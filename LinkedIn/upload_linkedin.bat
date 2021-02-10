458e187cb508a216ac6ede1f257e8ad0cce045da
Amanda Szampias <ASzampiasSWD@gmail.com>
2021-02-09 20:31:11 -0600

    bat file change

--- LinkedIn/upload_linkedin.bat
+++ LinkedIn/upload_linkedin.bat
@@ -1,4 +1,4 @@
-for /l %%x in (1, 1, 2834) do (
+for /l %%x in (0, 1, 2834) do (
    echo On file: %%x;
    curl -s -H "Content-Type: application/x-ndjson" -XPOST "localhost:9200/_bulk" --data-binary @linkedin_%%x.json > NUL;
 )
