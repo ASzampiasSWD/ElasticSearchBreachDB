3b19e064b9683dbe974a73543719433db5630199
Amanda Szampias <ASzampiasSWD@gmail.com>
2021-02-12 23:34:21 -0600

    'checker'

--- LinkedIn/checker.py
+++ LinkedIn/checker.py
@@ -5,21 +5,24 @@ import requests
 import json
 
 def getDocumentById(intCount):
-  x = requests.get('http://localhost:9200/breach/_doc/' + str(intCount))
+  x = requests.get('http://localhost:9200/breach-linkedin/_doc/' + str(intCount))
   y = json.loads(x.text)
   #print(y["found"]);
   return y["found"];
  
 def main():
   intCount = 0; # Change this value based on the index you started at.
-  for i in range(0, 2835):
+  bIfMissing = False
+  for i in range(0, 1645):
     bFound = getDocumentById(intCount)
     if (bFound == False):
+      bIfMissing = True
       print('File is missing: linkedin_' + str(i) + '.json')
-    else:
-      print('File is found: ' + str(i))
-    intCount = intCount + 29001;
+    #else:
+      #print('File is found: ' + str(i))
+    intCount = intCount + 50001
   print('-----------------------------')
+  print('Any Files Missing: ' + str(bIfMissing))
  
 if __name__ == "__main__":
   main()
