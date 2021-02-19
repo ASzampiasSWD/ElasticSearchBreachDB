d5c0285df1bd5207e4efec745b93dae66940699d
Amanda Szampias <ASzampiasSWD@gmail.com>
2021-02-18 20:20:56 -0600

    dropbox

--- Dropbox/checker.py
+++ Dropbox/checker.py
@@ -5,20 +5,19 @@ import requests
 import json
 
 def getDocumentById(intCount):
-  x = requests.get('http://localhost:9200/breach/_doc/' + str(intCount))
+  x = requests.get('http://localhost:9200/breach-dropbox/_doc/' + str(intCount))
   y = json.loads(x.text)
-  #print(y["found"]);
   return y["found"];
  
 def main():
-  intCount = 82209229; # Change this value based on the index you started at.
-  for i in range(0, 2835):
+  intCount = 0
+  for i in range(0, 1321):
     bFound = getDocumentById(intCount)
     if (bFound == False):
-      print('File is missing: linkedin_' + str(i) + '.json')
+      print('File is missing: dropbox_' + str(i) + '.json')
     else:
       print('File is found: ' + str(i))
-    intCount = intCount + 29001;
+    intCount = intCount + 52001;
   print('-----------------------------')
  
 if __name__ == "__main__":
--- Dropbox/upload_dropbox.bat
+++ Dropbox/upload_dropbox.bat
@@ -1,4 +1,4 @@
-for /l %%x in (0, 1, 2641) do (
+for /l %%x in (0, 1, 1320) do (
    echo Currently on file: %%x
    curl -s -H "Content-Type: application/x-ndjson" -XPOST "localhost:9200/_bulk" --data-binary @dropbox_%%x.json > NUL
 )
